716852ca8a83135c2e4ec330d390fe06300494b9
Meshack - <rutomeshack18@gmail.com>
2024-11-10 07:48:33 +0300

    Summarizer

--- cases/urls.py
+++ cases/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from .views import CaseList
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import UserRegister
+from . import views
 
 
 urlpatterns = [
@@ -10,4 +11,5 @@ urlpatterns = [
     path('password-reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),  
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('summarize/<str:case_number>/', views.summarize_case, name='summarize_case'),
 ]
--- cases/views.py
+++ cases/views.py
@@ -5,6 +5,10 @@ from .models import Case
 from .serializers import CaseSerializer
 from .serializers import UserSerializer
 from .models import CustomUser
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
+import requests
 
 
 def home(request):
@@ -30,3 +34,34 @@ class CaseList(generics.ListAPIView):
     filter_backends = (filters.DjangoFilterBackend, drf_filters.SearchFilter)
     filterset_class = CaseFilter
     search_fields = ['case_number', 'full_text']
+
+@api_view(['GET'])
+def summarize_case(request, case_number):
+    # Fetch the case by its case_number
+    case = get_object_or_404(Case, case_number=case_number)
+    
+    full_text = case.full_text
+    
+    # Hugging Face API URL and headers
+    api_url = "https://api-inference.huggingface.co/models/sshleifer/distilbart-cnn-12-6"
+    headers = {
+        "Authorization": "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" 
+    }
+    
+    response = requests.post(api_url, headers=headers, json={"inputs": full_text})
+    
+
+    if response.status_code == 200:
+        summary = response.json()[0]['summary_text']
+    else:
+        return Response({"error": "Failed to summarize the text"}, status=response.status_code)
+
+
+    serializer = CaseSerializer(case)
+    
+    return Response({
+        "case_number": case.case_number,
+        "summary": summary,
+        "case_data": serializer.data
+    })
+
